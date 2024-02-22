b017ded0478c6fbe1cb01e6b84e2640f2e8d4d26
João Martins <jvhotmart0@gmail.com>
2024-02-22 09:43:49 -0300

    second commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// const image = document.getElementById("product-image");
+// const nome = document.getElementById("product-name");
+// const price = document.getElementById("product-price");
+// const addButton = document.getElementById("add-button");
+
+// function addNewProduct(){
+//     `<section>
+//         <div class="product-container">
+//           <div id="">
+//             <img
+//               src="${image}"
+//               alt="${nome}"
+//               class="product-image"
+//             />
+//           </div>
+
+//           <div class="product-info">
+//             <p class="product-name">${nome}</p>
+//             <p class="product-price">R$${price}</p>
+//           </div>
+//           <button class="add-button">${addButton}</button>
+//         </div>
+//       </section>`
+// }
